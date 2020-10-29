0c398beb795d9338a996e40d43f9fa08b8297070
kmpm <me@kmpm.se>
2020-10-29 16:26:12 +0100

    docs: 2 GeoJSON generating examples

---
+++ examples/to_geojson_linestring.py
@@ -0,0 +1,32 @@
+import sys
+import json
+from pathlib import Path
+
+from sllib import Reader
+
+def main():
+    filename = sys.argv[1]
+    name = Path(filename).stem
+
+    coords = list()
+
+    with open(filename, 'rb') as f:
+        reader = Reader(f)
+        print(reader.header)
+        last = None
+        for frame in reader:
+            c = (frame.longitude, frame.latitude)
+            if c != last:
+                coords.append(c)
+                last = c
+
+    line = dict(type='Feature',
+                geometry=dict(type='LineString', coordinates=coords),
+                properties=dict())
+    data = dict(type='FeatureCollection', features=[line])
+    with open(f'{name}.json', 'w') as outfile:
+        json.dump(data, outfile, indent=4)
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/to_geojson_points.py
@@ -0,0 +1,33 @@
+import sys
+import json
+from pathlib import Path
+
+from sllib import Reader
+
+def main():
+    filename = sys.argv[1]
+    name = Path(filename).stem
+
+    features = list()
+
+    with open(filename, 'rb') as f:
+        reader = Reader(f)
+        print(reader.header)
+        last = None
+        for frame in reader:
+            c = (frame.longitude, frame.latitude)
+            point = dict(type='Feature',
+                         geometry=dict(type='Point', coordinates=c),
+                         properties=dict(water_depth_m=frame.water_depth_m,
+                                         speed_kph=frame.gps_speed_kph))
+            if c != last:
+                features.append(point)
+                last = c
+
+    data = dict(type='FeatureCollection', features=features)
+    with open(f'{name}.json', 'w') as outfile:
+        json.dump(data, outfile, indent=4)
+
+
+if __name__ == "__main__":
+    main()
