363d4312a345bce2ec693c17d40fb9f03a78b7a6
kmpm <me@kmpm.se>
2021-09-11 12:21:32 +0200

    docs: improve csv example

--- examples/tocsv.py
+++ examples/tocsv.py
@@ -1,54 +1,60 @@
 import csv
 import glob
 import os
-import sys
 from pathlib import Path
+import argparse
 
 from sllib import Reader
 
-FIELDS = ['time1', 'gps_speed', 'gps_speed_kph', 'lon_enc', 'lat_enc',
-          'longitude', 'latitude', 'water_depth_m', 'packetsize']
+
+def create_csv_with_header(csvfile, fields):
+    writer = csv.DictWriter(
+        csvfile, fields, dialect='excel',
+        extrasaction='ignore', delimiter='\t')
+    writer.writeheader()
+    return writer
 
 
-def process_file(filename, outpath):
+def process_file(filename, outpath, all):
     name = Path(filename).stem
-    outfile = os.path.join(outpath, f'{name}.csv')
+    outfile = os.path.join(outpath, f'{name}.csv.tab')
     with open(outfile, 'w', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, FIELDS, dialect='excel',
-                                extrasaction='ignore', delimiter='\t')
-        writer.writeheader()
         last = None
         with open(filename, 'rb') as f:
             reader = Reader(f)
-            print(reader.header)
-            first = True
+            print(filename, reader.header)
+            fields = reader.header.fields
+            writer = create_csv_with_header(csvfile,  fields)
             for frame in reader:
-                if first:
-                    print(frame.packetsize)
-                    first = False
                 point = (frame.longitude, frame.latitude)
-                if point != last:
-                    writer.writerow(frame.to_dict(fields=FIELDS))
+                if all or point != last:
+                    writer.writerow(frame.to_dict(fields=fields, format=reader.header.format))
                     last = point
 
 
-def main():
-    filepath = sys.argv[1]
-    print(f'Testing {filepath} to see what it is')
-    if os.path.isfile(filepath):
+def main(path, all):
+    print(f'Testing {path} to see what it is')
+    if os.path.isfile(path):
         print('You provided a file.')
-        process_file(filepath, os.path.dirname(filepath))
-    elif os.path.isdir(filepath):
-        pattern = os.path.join(filepath, '*.sl*')
-        print('Gou provided a directory.')
+        process_file(path, os.path.dirname(path), all)
+    elif os.path.isdir(path):
+        pattern = os.path.join(path, '*.sl*')
+        print('You provided a directory.')
         print("Will try to glob " + pattern)
         for filename in glob.iglob(pattern):
             print(filename)
-            process_file(filename, filepath)
+            process_file(filename, path)
     else:
-        print(f'Error! You must provide a file or directory. "{filepath}" is neither.')
+        print(f'Error! You must provide a file or directory. "{path}" is neither.')
 
 
 if __name__ == "__main__":
-    # print('asdfasdf')
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help="path to file or directory to process.")
+    parser.add_argument(
+        '-a', '--all',
+        help="store all records even if position is unchanged",
+        action='store_true')
+    args = parser.parse_args()
+    print(args)
+    main(args.path, args.all)
