4febf854a7cd74d9be98713f2db7664c9b0b222a
kmpm <me@kmpm.se>
2021-09-12 11:48:19 +0200

    chore: change release stuff for 0.2.1

--- setup.py
+++ setup.py
@@ -1,27 +1,4 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup(
-    name='sllib',
-    python_requires='>3.6.0',
-    version='0.2.1',
-    description='Library for reading SLG or SL2 files created by Lowrance fishfinders',
-    url='http://github.com/opensounder/python-sllib',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.9",
-    ],
-    author='Peter Magnusson',
-    author_email='me@kmpm.se',
-    license='MIT',
-    packages=['sllib'],
-    install_requires=[],
-    test_suite='tests',
-    zip_safe=False)
+if __name__ == "__main__":
+    setup()
